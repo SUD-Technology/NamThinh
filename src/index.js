19896a4f6d2e091c1bfd1439a5efe2c6b1e2054b
Dung <chungle8216@gmail.com>
2022-07-13 12:30:46 +0700

    fix xbug

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-yconst express = require('express')
+const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
 const handlebars = require('express-handlebars')
