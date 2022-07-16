4f64355d20b18980f7f47e540f68dcafdee89de4
Dung <chungle8216@gmail.com>
2022-07-16 10:45:36 +0700

    Add Products

--- src/resources/controllers/UserController.js
+++ src/resources/controllers/UserController.js
@@ -2,6 +2,14 @@
 const UserController = {
     getLogin: (req, res, next) => {
         return res.render('login');
+    },
+
+    postLogin: (req, res, next) => {
+        const { username, password } = req.body;
+
+        if(username == 'admin' && password == '123456') {
+            return res.redirect('/admin/product-manager');
+        }
     }
 }
 
