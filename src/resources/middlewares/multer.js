325589359ddac27972884790c4319f8d69758b69
Tran Khai Hoang <hoangbo74@gmail.com>
2022-08-18 11:20:21 +0700

    change multer destinaiton

--- src/resources/middlewares/multer.js
+++ src/resources/middlewares/multer.js
@@ -1,11 +1,7 @@
 const multer = require('multer')
 let storage = multer.diskStorage({
   destination: function (req, file, cb) {
-<<<<<<< HEAD
-    // cb(null, 'source/src/public/uploads')
-=======
->>>>>>> 441af7dd571e63c600df95991091dd5706dc3894
-    cb(null, 'src/public/uploads')
+    cb(null, 'source/src/public/uploads')
   },
   filename: function (req, file, cb) {
     let ext = file.originalname.substring(file.originalname.lastIndexOf('.'))
