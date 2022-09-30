6d10d64de6808c267a2fb6d742a288077f8c88fc
Sheldon Kuo <nkeyskuo124@gmail.com>
2022-09-30 07:24:03 +0700

    fix

--- src/resources/middlewares/multer.js
+++ src/resources/middlewares/multer.js
@@ -2,7 +2,7 @@ const multer = require('multer')
 
 let storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'source/src/public/uploads')
+    cb(null, 'src/public/uploads')
   },
   filename: function (req, file, cb) {
     let ext = file.originalname.substring(file.originalname.lastIndexOf('.'))
